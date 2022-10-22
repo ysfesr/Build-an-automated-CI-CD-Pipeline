6bb127a912bc28f49324898c0327d692fd2cbd11
Youssef ELASERY <joseph.elasery@gmail.com>
2022-10-22 09:34:09 +0100

    toDoList

---
+++ todolist/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("", include("lists.urls")),
+    path("auth/", include("accounts.urls")),
+    path("api/", include("api.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+    path("admin/", admin.site.urls),
+]
