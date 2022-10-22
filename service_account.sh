1a92686c77d5c758ab69113889d0c07701988e30
Youssef ELASERY <joseph.elasery@gmail.com>
2022-10-22 18:26:34 +0100

    Service Account creation

---
+++ service_account.sh
@@ -0,0 +1,21 @@
+export PROJECT_ID=""
+export ACCOUNT_NAME=""
+
+gcloud services enable cloudbuild.googleapis.com run.googleapis.com containerregistry.googleapis.com
+
+gcloud iam service-accounts create $ACCOUNT_NAME --display-name="CloudRun-SA"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:$ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com \
+--role=roles/run.admin
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member=serviceAccount:$ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com \
+  --role=roles/storage.admin
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member=serviceAccount:$ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com \
+  --role=roles/iam.serviceAccountUser
+
+  gcloud iam service-accounts keys create key.json \
+    --iam-account $ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com
